0650264364ca48c662981d3ce903e5698dfccdc4
GabrielDeml <gabrielddeml@gmail.com>
2022-01-21 15:39:15 -0500

    It works!

--- main.py
+++ main.py
@@ -5,28 +5,33 @@ import copy
 
 def check_if_word_is_possable(word, set_letters, wild_letters, bad_letters):
 	# Get the number of unique letters in wild_letters dict
-	unknown_letters = len(word) - len(wild_letters) - len(set_letters)
-	set_letters = copy.deepcopy(set_letters)
-	wild_letters = copy.deepcopy(wild_letters)
+	set_letters_count = 0
+	wild_letters_count = 0
+	set_letters = set_letters.copy()
+	wild_letters = wild_letters.copy()
 	
 
 	# Check if the word is possable
 	for letter_pos in range(len(word)):
 		letter = word[letter_pos]
 
-		if letter_pos in set_letters.keys() and letter != set_letters[letter_pos]:
+		if letter_pos in set_letters.keys():
+			set_letters_count += 1
+			if letter in wild_letters.keys():
+				wild_letters_count += 1
+			if letter != set_letters[letter_pos]:
 				return False
 		elif letter in wild_letters.keys():
 			for letter_pos_tmp in wild_letters[letter]:
 				if letter_pos == letter_pos_tmp:
 					return False
+			wild_letters_count += 1
 		elif letter in bad_letters:
 			return False
-		else:
-			unknown_letters -= 1
-			if unknown_letters < 0:
-				return False
-	return True
+	if set_letters_count == len(set_letters) and wild_letters_count >= len(wild_letters):
+		return True
+	else:
+		return False
 		
 
 def find_best_choice(letters_in_position, words, set_letters, wild_letters, bad_letters):
@@ -91,7 +96,7 @@ if __name__ == '__main__':
 	tried_word = find_best_choice(letters_in_position, words, set_letters, wild_letters, bad_letters)
 	print(tried_word)
 	print("Use _ as spaces")
-	for i in range(len(words[0]) + 1): 
+	for i in range(len(words[0])): 
 		# Get user input
 		green_user_input = input('Enter Green letters: ')
 		yellow_user_input = input('Enter Yellow letters: ')
